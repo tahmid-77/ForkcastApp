5d8c3b8c9ba4aec9c680f9ab7997c270228c5627
Tahmid Ahmed <tahmidahmed8798@gmail.com>
2025-05-14 20:27:29 +1000

    Structure

---
+++ Forkcast/ForkcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForkcastApp.swift
+//  ForkcastApp
+//
+//  Created by Tahmid Ahmed on 8/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct ForkcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
